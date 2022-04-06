d360f1963295ff0a9da48e9ba7d71d15f8b663d5
Tom KELDENICH <tom.keldenich@valeo.com>
2022-04-06 15:49:34 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { createStore, applyMiddleware } from "redux";
+//import reportWebVitals from "./reportWebVitals";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-//import { getFirestore } from "redux-firestore";
+import { getFirestore } from "firebase/firestore";
+//import { reduxFirestore, getFirestore, createFirestoreInstance } from "redux-firestore";
+import { reduxFirestore, createFirestoreInstance } from "redux-firestore";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+import firebase from "firebase/compat/app";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(fbConfig, {}),
+  )
+);
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
 
 /* const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,15 +39,17 @@ root.render(
 ); */
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  //<React.StrictMode>
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  //</React.StrictMode>,
   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,23 @@
+import { collection, addDoc } from 'firebase/firestore'
+
 export const createProject = (project) => {
-  return (dispatch, getState) => {
-    // make async call to database
-    dispatch({ type: "CREATE_PROJECT", project });
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const db = getFirestore();
+    // add document to collection
+    // find here : https://firebase.google.com/docs/firestore/manage-data/add-data#web-version-9
+    // Add a document - without ID
+    addDoc(collection(db, "projects"), {
+      /* title: project.title,
+      content: project.content, =*/
+      ...project,
+      authorFirstName: 'Net',
+      authorLastName: 'Ninja',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: "CREATE_PROJECT", project });
+    }).catch((err) => {
+      dispatch({ type: "CREATE_PROJECT_ERROR", err });
+    })    
   };
 };
